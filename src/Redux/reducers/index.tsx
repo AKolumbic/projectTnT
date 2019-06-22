bdc5971dab17438544acda7e86350de23b71dd47
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2019-06-22 19:43:45 -0400

    initial commit

---
+++ src/Redux/reducers/index.tsx
@@ -0,0 +1,7 @@
+const initialState = {}
+
+export default function rootReducer(state = initialState, action: any) {
+    // For now, don't handle any actions
+    // and just return the state given to us.
+    return state
+}
