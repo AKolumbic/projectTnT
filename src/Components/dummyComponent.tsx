67b66f7b8f51edf5fa03cd54ef17a11ca599ef63
Andrew Kolumbic <spguy13jamc@sbcglobal.net>
2019-06-22 20:20:59 -0400

    working hello world boilerplate

---
+++ src/Components/dummyComponent.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+interface IProps {
+  name?: string;
+}
+
+const DummyComponent: React.FC<IProps> = (props: IProps) => (
+  <h1>Hello, {props.name}! Welcome to React and TypeScript.</h1>
+)
+
+DummyComponent.defaultProps = {
+  name: 'world',
+}
+
+export default DummyComponent
